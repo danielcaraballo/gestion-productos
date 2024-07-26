b9968513489d00a8fb0326f560a02e859c279d08
Daniel Caraballo <danielcaraballoponte@gmail.com>
2024-07-26 13:46:03 -0400

    feat(admin): añadir models app productos al admin

--- apps/productos/admin.py
+++ apps/productos/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models        import (Categoria, Tecnologia, Solicitante, Responsable, ServidorWeb, BaseDatos,
+                            Version, Producto, TecnologiaProducto, ResponsableProducto, Infraestructura)
 
 # Register your models here.
+
+admin.site.register(Categoria)
+admin.site.register(Tecnologia)
+admin.site.register(Solicitante)
+admin.site.register(Responsable)
+admin.site.register(ServidorWeb)
+admin.site.register(BaseDatos)
+admin.site.register(Version)
+admin.site.register(Producto)
+admin.site.register(TecnologiaProducto)
+admin.site.register(ResponsableProducto)
+admin.site.register(Infraestructura)
