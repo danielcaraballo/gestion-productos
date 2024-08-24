9661c4aef1629c69bf161dd765663ab870269d1d
Daniel Caraballo <danielcaraballoponte@gmail.com>
2024-08-23 22:24:00 -0400

    refactor(config): renombrar carpeta de configuracion del proyecto a config

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for gestion_productos project.
+WSGI config for gestion-productos project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestion_productos.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
