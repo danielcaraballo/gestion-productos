fa827509128fc7f043c4f141e73a47d7310479d8
Daniel Caraballo <danielcaraballoponte@gmail.com>
2024-08-30 21:10:01 -0400

    feat(API): anadir swagger para documentar la API

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API - GESTION PRODUCTOS",
+        default_version='v1',
+        description="API de la Aplicacion de Gestion de Productos, desarrollada por Daniel Caraballo",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,5 +37,6 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/productos/', include('apps.productos.urls')),
     path('api/usuarios/', include('apps.usuarios.urls')),
-    
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'), 
 ]
